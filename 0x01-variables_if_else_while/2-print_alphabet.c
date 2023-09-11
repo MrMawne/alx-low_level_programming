f07ee022c8db39b336ecfc77b59c3b7262854749
MrMawne <you@example.com>
2023-09-11 06:07:27 -0700

    great

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - entry poin
+ *
+ * Description: print alphabet using putchar only
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	ch++;
+	}
+
+	return 0;
+}
