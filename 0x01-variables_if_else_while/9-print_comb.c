334dbb070c2a3c2ed9b02a99a824cdeb20a52d29
MrMawne <you@example.com>
2023-09-11 19:59:53 -0700

    great

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program that prints all possible numbers
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
