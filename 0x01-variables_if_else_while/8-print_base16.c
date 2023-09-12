4a544feda26957f3e315bae6116ad440160dd815
MrMawne <you@example.com>
2023-09-11 19:50:44 -0700

    great

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,15 @@
 int main(void)
 {
 	int n;
+	int m;
 
-	for (n = 0; n <= 15; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		n++;
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
 	}
 	putchar('\n');
 
