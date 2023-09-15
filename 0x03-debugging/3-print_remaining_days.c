2c7a808b1badaafd400bdbdae95e3a57b00c17b4
MrMawne <you@example.com>
2023-09-14 18:54:38 -0700

    push

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,6 +18,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 
@@ -32,6 +33,8 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
+		}
+		}
 		}
 	}
 }
